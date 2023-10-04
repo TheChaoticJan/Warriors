ee46de534a705b0840e3e337c168e87fc8a3f61f
TheChaoticJan <jheymann2912@gmail.com>
2023-10-04 00:16:18 -0700

    Alpha 0.2.4.3
    Removed some Redundancies
    Made some Code Easier to read through
    (-> Simplified if clauses for reading)

--- Sci-Fi-Skirmish-main/JansParadise/src/main/java/plugin/Infobar/Actionbar.java
+++ Sci-Fi-Skirmish-main/JansParadise/src/main/java/plugin/Infobar/Actionbar.java
@@ -8,12 +8,12 @@ public class Actionbar {
 
 
     public static String Actionbar(Player p, PlayerStats stats, Player d, int module1, int module2, int module3){
-        String Actionbar = "";
 
         int HDura = Count.CountValues(p).get(4);
         int CDura = Count.CountValues(p).get(5);
         int LDura = Count.CountValues(p).get(6);
         int BDura = Count.CountValues(p).get(7);
+
         String Piece = "";
 
             if(!(p.getInventory().getLeggings() == null) && !(p.getInventory().getBoots() == null)  && !(p.getInventory().getChestplate() == null)  && !(p.getInventory().getHelmet() == null)){
@@ -22,18 +22,18 @@ public class Actionbar {
                     Piece =  "§8<§4Hose§8>§c " + (i + (p.getInventory().getLeggings().getMaxItemUseDuration() - p.getInventory().getLeggings().getDurability()));
 
                 }else
-                if(BDura <= HDura && BDura <= CDura && BDura <= LDura){
+                if(BDura <= HDura && BDura <= CDura){
                     int i = p.getInventory().getBoots().getType().getMaxDurability();
                     Piece =  "§8<§4Schuhe§8>§c " + (i + (p.getInventory().getBoots().getMaxItemUseDuration() - p.getInventory().getBoots().getDurability()));
                 }else
                 if(CDura <= LDura && CDura <= BDura && CDura <= HDura) {
                     int i = p.getInventory().getChestplate().getType().getMaxDurability();
                     Piece = "§8<§4Chest§8>§c " + (i + (p.getInventory().getChestplate().getMaxItemUseDuration() - p.getInventory().getChestplate().getDurability()));
-                }        else
-                if(HDura <= BDura && HDura <= LDura && HDura <= CDura){
+                }else{
                     int i = p.getInventory().getHelmet().getType().getMaxDurability();
                     Piece = "§8<§4Helm§8>§c " + (i + (p.getInventory().getHelmet().getMaxItemUseDuration() - p.getInventory().getHelmet().getDurability()));
-                }}
+                }
+            }
             else {
                 Piece ="§c" + Math.round(p.getHealth()/2) + " §c❤";
             }
@@ -41,6 +41,7 @@ public class Actionbar {
         if(stats.getClan() == ""){
                 stats.setClan("§cClanlos");
             }
+        
         String clan = "§8<§6Konto§8> §e" + stats.getXp() + " §6✧";
         String xp1 = "§8<§aXP§8> §2" + Count.CountValues(p).get(0);
         String pearls1 = "§8<§5Pearls§8> §x§D§6§5§B§E§9" + Count.CountValues(p).get(1);
@@ -53,48 +54,29 @@ public class Actionbar {
         String piece2 = "";
         String piece3 = "";
 
-        if(module1 == 1){
-            piece1 = Piece;
-        }else if(module1 == 2){
-            piece1 = xp1;
-        }else if(module1 == 3){
-            piece1 = pearls1;
-        }else if(module1 == 4){
-            piece1 = clan;
-        }else if(module1 == 5){
-            piece1 = tnt1;
-        }else if(module1 == 6){
-            piece1 = webs1;
+        for(int i = 1; i <= 3; i++){
+            String var = "";
+            int val = 0;
+            //Gathering moduleX
+            if(i == 1) { val = module1; }
+            if(i == 2) { val = module2; }
+            if(i == 3) { val = module3; }
+
+            if(val == 1){ var = Piece; }
+            else if(val == 2){ var = xp1; }
+            else if(val == 3){ var = pearls1; }
+            else if(val == 4){ var = clan; }
+            else if(val == 5){ var = tnt1; }
+            else if(val == 6){ var = webs1; }
+            //Assigning to pieceX
+            if(i == 1) { piece1 = var; }
+            if(i == 2) { piece2 = var; }
+            if(i == 3) { piece3 = var; }
         }
 
-        if(module2 == 1){
-            piece2 = Piece;
-        }else if(module2 == 2){
-            piece2 = xp1;
-        }else if(module2 == 3){
-            piece2 = pearls1;
-        }else if(module2 == 4){
-            piece2 = clan;
-        }else if(module2 == 5){
-            piece2 = tnt1;
-        }else if(module2 == 6){
-            piece2 = webs1;
-        }
-
-        if(module3 == 1){
-            piece3 = Piece;
-        }else if(module3 == 2){
-            piece3 = xp1;
-        }else if(module3 == 3){
-            piece3 = pearls1;
-        }else if(module3 == 4){
-            piece3 = clan;
-        }else if(module3 == 5){
-            piece3 = tnt1;
-        }else if(module3 == 6){
-            piece3 = webs1;
-        }
-
+        String Actionbar = "";
         Actionbar = piece1 + " §7| " + piece2 + " §7| " + piece3;
-   return Actionbar;
-}}
+
+        return Actionbar;
+    }
+}
